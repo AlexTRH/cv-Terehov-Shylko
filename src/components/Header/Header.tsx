751d6508fe22593c934e6e6437aa2d0634aced7c
AlexTRH <19tam99@mail.ru>
2023-09-07 11:20:21 +0300

    Fixed bugs according to comments

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,38 @@
 import React, { useState, startTransition, useEffect } from 'react'
-import { useReactiveVar } from '@apollo/client';
-import { authService } from '../../graphql/auth/auth.service';
+import { useReactiveVar } from '@apollo/client'
+import { authService } from '../../graphql/auth/auth.service'
 import { AppBar, Toolbar, Tabs, Tab } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
 import { Link } from 'react-router-dom'
-import { StyledBox, StyledIconButton, StyledToolbar } from './header.styles'
-import { RoutesPath } from '../../constants/routes.constants';
-import SideMenu from '../organisms/side-menu/side-menu.organism';
-import { UserMenu } from '../user-menu/user-menu';
+import {
+  StyledBox,
+  StyledIconButton,
+  StyledToolbar,
+} from '../../components/Header/header.styles'
+import { RoutesPath } from '../../constants/routes.constants'
+import SideMenu from '../organisms/side-menu/side-menu.organism'
+import { UserMenu } from '../user-menu/user-menu'
 
 const Header = () => {
-  const isAuth = useReactiveVar(authService.access_token$);
+  const isAuth = useReactiveVar(authService.access_token$)
   const [value, setValue] = useState('LOGIN')
-   const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     startTransition(() => {
-      setValue(newValue);
-    });
-  };
+      setValue(newValue)
+    })
+  }
 
-   const openMenu = () => {
+  const openMenu = () => {
     setIsOpen(true)
-  };
+  }
 
   const closeMenu = () => {
     setTimeout(() => {
       setIsOpen(false)
-    }, 90);
-  };
+    }, 90)
+  }
 
   return (
     <AppBar position="static">
@@ -49,7 +53,7 @@ const Header = () => {
       ) : (
         <Toolbar>
           <StyledBox>
-              <Tabs
+            <Tabs
               value={value}
               onChange={handleChange}
               indicatorColor="secondary"
