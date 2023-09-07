64ba3869b15a2b2fca76b5d3a805971d3cd5bc4e
AlexTRH <19tam99@mail.ru>
2023-09-07 18:30:24 +0300

    Created profile page

---
+++ src/components/organisms/employee-profile-form/employee-profile-form.organism.tsx
@@ -0,0 +1,69 @@
+import { useForm, FormProvider } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
+import { Button, TextField } from '@mui/material'
+import { DepartmentSelect } from '../../molecules/department-select'
+import { PositionSelect } from '../../molecules/position-select'
+import { useUserUpdate } from '../../../hooks/use-users.hook'
+import { IUser } from '../../../interfaces/user.interface'
+import { useUser } from '../../../hooks/use-user.hook'
+import {
+  EmployeeProfileFormProps,
+  EmployeeProfileFormValues,
+} from './employee-profile-form.types'
+import * as Styled from './employee-profile-form.styles'
+
+export const EmployeeProfileForm = ({ user }: EmployeeProfileFormProps) => {
+  const [updateUser, loading] = useUserUpdate()
+  const { user$, isAdmin } = useUser()
+  const { t } = useTranslation()
+
+  const setDefaultValues = (user: IUser) => ({
+    first_name: user.profile.first_name || '',
+    last_name: user.profile.last_name || '',
+    department: user.department?.id || '',
+    position: user.position?.id || '',
+  })
+
+  const methods = useForm<EmployeeProfileFormValues>({
+    defaultValues: setDefaultValues(user),
+  })
+  const { formState, register, handleSubmit, reset } = methods
+
+  const onSubmit = (values: EmployeeProfileFormValues) => {
+    updateUser({
+      variables: {
+        id: user.id,
+        user: {
+          profile: {
+            first_name: values.first_name,
+            last_name: values.last_name,
+          },
+          departmentId: values.department,
+          positionId: values.position,
+          role: user.role,
+        },
+      },
+    }).then(({ data }) => data && reset(setDefaultValues(data.updateUser)))
+  }
+
+  return (
+    <FormProvider {...methods}>
+      <Styled.Form onSubmit={handleSubmit(onSubmit)}>
+        <TextField {...register('first_name')} label={t('First Name')} />
+        <TextField {...register('last_name')} label={t('Last Name')} />
+        <DepartmentSelect name="department" />
+        <PositionSelect name="position" />
+        {(isAdmin || user$?.id === user.id) && (
+          <Button
+            type="submit"
+            variant="contained"
+            disabled={!formState.isDirty || loading}
+            sx={{ gridColumn: 2 }}
+          >
+            {t('Update')}
+          </Button>
+        )}
+      </Styled.Form>
+    </FormProvider>
+  )
+}
---
+++ src/pages/employee-profile/employee-profile.page.tsx
@@ -0,0 +1,39 @@
+import { memo } from 'react'
+import { useParams } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { useQuery } from '@apollo/client'
+import { Typography } from '@mui/material'
+import { getUserQuery } from '../../graphql/users/users.queries'
+import { UserResult } from '../../graphql/users/users.types'
+import { AvatarUpload } from '../../components/molecules/avatar-upload'
+import { EmployeeProfileForm } from '../../components/organisms/employee-profile-form'
+import * as Styled from './employee-profile.styles'
+
+const EmployeeProfile = () => {
+  const { id } = useParams()
+  const { t } = useTranslation()
+  const { data, loading } = useQuery<UserResult>(getUserQuery, {
+    variables: { id },
+  })
+
+  if (loading || !data) {
+    // TODO: add loader
+    return null
+  }
+
+  return (
+    <>
+      <AvatarUpload user={data.user} />
+      <Styled.Profile>
+        <Typography variant="h5">{data.user.profile.full_name}</Typography>
+        <Typography>{data.user.email}</Typography>
+        <Typography>
+          {t('A member since')} {new Date(+data.user.created_at).toDateString()}
+        </Typography>
+      </Styled.Profile>
+      <EmployeeProfileForm user={data.user} />
+    </>
+  )
+}
+
+export default memo(EmployeeProfile)
