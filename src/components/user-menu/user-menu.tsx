ffc006e890cf18f31f2e111f97b1261709ffa8d6
AlexTRH <19tam99@mail.ru>
2023-09-14 11:15:06 +0300

    Bug fixes

--- src/components/user-menu/user-menu.tsx
+++ src/components/user-menu/user-menu.tsx
@@ -1,82 +1,78 @@
-import { useQuery, useReactiveVar } from '@apollo/client';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import Logout from '@mui/icons-material/Logout';
-import SettingsIcon from '@mui/icons-material/Settings';
-import { Divider, IconButton, Menu, MenuItem, Typography } from '@mui/material';
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useQuery, useReactiveVar } from '@apollo/client'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import Logout from '@mui/icons-material/Logout'
+import SettingsIcon from '@mui/icons-material/Settings'
+import { Divider, IconButton, Menu, MenuItem, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { RoutesPath } from '../../constants/routes.constants'
 
-import { authService } from '../../graphql/auth/auth.service';
-import { UserResult } from '../../graphql/user/user.types';
-import { User } from '../../graphql/user/user.queries';
-import { IconStyles, StyledAvatar, UserMenuWrap } from './user-menu.styles';
+import { authService } from '../../graphql/auth/auth.service'
+import { UserResult } from '../../graphql/users/users.types'
+import { getUserQuery } from '../../graphql/users/users.queries'
+import { IconStyles, StyledAvatar, UserMenuWrap } from './user-menu.styles'
 
 export const UserMenu = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement | undefined>(
-    null
-  );
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement | undefined>(null)
 
-  const userData = useReactiveVar(authService.user$);
+  const userData = useReactiveVar(authService.user$)
 
-  const { data } = useQuery<UserResult>(User, {
-    variables: { id: userData?.id }
-  });
-  const user = data?.user;
+  const { data } = useQuery<UserResult>(getUserQuery, {
+    variables: { id: userData?.id },
+  })
+  const user = data?.user
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
+    setAnchorEl(event.currentTarget)
+  }
 
   const handleClose = () => {
-    setAnchorEl(null);
-  };
+    setAnchorEl(null)
+  }
 
   const handleLogout = () => {
-    authService.logout();
+    authService.logout()
     navigate(RoutesPath.Login)
-  };
+  }
 
   const openProfile = () => {
-    navigate(RoutesPath.Employee_profile.replace(':id', `${user?.id}`), { replace: true });
-  };
+    navigate(RoutesPath.Employee_profile.replace(':id', `${user?.id}`), {
+      replace: true,
+    })
+  }
 
   return (
-      <>
-        <UserMenuWrap>
-          <Typography>{user?.profile.full_name || user?.email}</Typography>
-          <IconButton onClick={handleClick} size="large">
-            <StyledAvatar src={user?.profile.avatar} alt="user avatar">
-              {user?.email[0].toUpperCase()}
-            </StyledAvatar>
-          </IconButton>
-        </UserMenuWrap>
-        {!!anchorEl && (
-          <Menu
-            anchorEl={anchorEl}
-            open={!!anchorEl}
-            onClick={handleClose}
-          >
-            <MenuItem onClick={openProfile}>
-              <AccountCircleIcon sx={IconStyles} />
-              Profile
-            </MenuItem>
+    <>
+      <UserMenuWrap>
+        <Typography>{user?.profile.full_name || user?.email}</Typography>
+        <IconButton onClick={handleClick} size="large">
+          <StyledAvatar src={user?.profile.avatar} alt="user avatar">
+            {user?.email[0].toUpperCase()}
+          </StyledAvatar>
+        </IconButton>
+      </UserMenuWrap>
+      {!!anchorEl && (
+        <Menu anchorEl={anchorEl} open={!!anchorEl} onClick={handleClose}>
+          <MenuItem onClick={openProfile}>
+            <AccountCircleIcon sx={IconStyles} />
+            Profile
+          </MenuItem>
 
-            <MenuItem>
-              <SettingsIcon sx={IconStyles} />
-              Setting
-            </MenuItem>
+          <MenuItem>
+            <SettingsIcon sx={IconStyles} />
+            Setting
+          </MenuItem>
 
-            <Divider />
+          <Divider />
 
-            <MenuItem onClick={handleLogout}>
-              <Logout sx={IconStyles} />
-              Logout
-            </MenuItem>
-          </Menu>
-        )}
-      </>
-  );
-};
+          <MenuItem onClick={handleLogout}>
+            <Logout sx={IconStyles} />
+            Logout
+          </MenuItem>
+        </Menu>
+      )}
+    </>
+  )
+}
