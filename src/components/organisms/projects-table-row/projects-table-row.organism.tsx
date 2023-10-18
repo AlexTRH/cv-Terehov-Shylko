ddee660eb191d96122923a072667733ec827a05e
nastassia2020 <wcganastacia@yandex.ru>
2023-10-18 17:34:56 +0300

    added notification for user on projects page

--- src/components/organisms/projects-table-row/projects-table-row.organism.tsx
+++ src/components/organisms/projects-table-row/projects-table-row.organism.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useMutation } from '@apollo/client'
 import { MenuItem, TableCell, TableRow } from '@mui/material'
 import { ActionsMenu }from '../../atoms/actions-menu'
@@ -5,16 +6,20 @@ import { TableRowProps } from '../../templates/table/table.types'
 import { getDeleteProjectMutation, getProjectsQuery } from '../../../graphql/projects/projects.queries'
 import { IProject } from '../../../interfaces/project.interface'
 import { useUser } from '../../../hooks/use-user.hook'
+import { Notifications } from '../../features/notifications'
 
 export const ProjectsTableRow = ({ item }: TableRowProps<IProject>) => {
   const { isAdmin } = useUser()
+
+  const [errorMessage, setErrorMessage] = useState(false)
 
   const [DeleteProject] = useMutation<{ affected: number }>(getDeleteProjectMutation, {
     refetchQueries: [{ query: getProjectsQuery }],
     onError: (error) => {
       console.error('An error occurred during the mutation:', error)
+      setErrorMessage(true);
     }
-  })
+  });
 
   const handleDelete = () => {
     DeleteProject({
@@ -23,6 +28,8 @@ export const ProjectsTableRow = ({ item }: TableRowProps<IProject>) => {
   }
 
   return (
+    <>
+    {errorMessage && <Notifications />}
       <TableRow>
         <TableCell>{item.name}</TableCell>
         <TableCell sx={{ textAlign: 'right' }}>
@@ -33,5 +40,7 @@ export const ProjectsTableRow = ({ item }: TableRowProps<IProject>) => {
           </ActionsMenu>
         </TableCell>
       </TableRow>
+    </>
+      
   )
 }
