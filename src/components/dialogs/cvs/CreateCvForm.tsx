707e7a41c6f6fa8ef6bd32ace7fba352d07398d2
nastassia2020 <wcganastacia@yandex.ru>
2023-10-23 13:56:04 +0300

    fixed according to comments

--- src/components/dialogs/cvs/CreateCvForm.tsx
+++ src/components/dialogs/cvs/CreateCvForm.tsx
@@ -1,50 +1,63 @@
+import { useState } from 'react'
 import { useMutation, useQuery, useReactiveVar } from '@apollo/client'
 import CloseIcon from '@mui/icons-material/Close'
 import { Button, Dialog, DialogContent, IconButton } from '@mui/material'
 import { useForm } from 'react-hook-form'
 import { authService } from '../../../graphql/auth/auth.service'
 import { createCv, getCVsQuery } from '../../../graphql/cvs/cvs.queries'
-import { User } from '../../../graphql/user/user.queries'
+import { getUserQuery } from '../../../graphql/users/users.queries'
 import { StyledBox, StyledDialogTitle } from './CreateCvForm.styles'
 import FormFields from './FormFields'
-import { CreateCvFormInput, CreateCvFormProps, UserAllResult } from './CreateCvForm.types'
+import {
+  CreateCvFormInput,
+  CreateCvFormProps,
+  UserAllResult,
+} from './CreateCvForm.types'
+import { Notifications } from '../../features/notifications'
+import Preloader from '../../atoms/preloader/preloader.atom'
 
-
-const CreateCvForm: React.FC<CreateCvFormProps> = ({ close, confirm, opened }) => {
+const CreateCvForm: React.FC<CreateCvFormProps> = ({
+  close,
+  confirm,
+  opened,
+}) => {
+  const [errorMessage, setErrorMessage] = useState('')
   const { control, handleSubmit, reset } = useForm<CreateCvFormInput>()
   const user = useReactiveVar(authService.user$)
 
-  const { data: userData } = useQuery<UserAllResult>(
-    User,
-    {
-      variables: { id: user?.id },
-    }
-  )
+  const { data: userData } = useQuery<UserAllResult>(getUserQuery, {
+    variables: { id: user?.id },
+  })
 
-  const [createCV, { loading }] = useMutation(createCv, {
+  const [createCV, { loading, error }] = useMutation(createCv, {
     refetchQueries: [{ query: getCVsQuery }],
   })
 
   const onSubmit = async (inputs: CreateCvFormInput) => {
-    await createCV({
-      variables: {
-        cv: {
-          name: inputs.name,
-          description: inputs.description,
-          userId: userData?.user.id,
-          skills: [],
-          projectsIds: [],
-          languages: [],
-          is_template: inputs.template,
+    try {
+      await createCV({
+        variables: {
+          cv: {
+            name: inputs.name,
+            description: inputs.description,
+            userId: userData?.user.id,
+            skills: [],
+            projectsIds: [],
+            languages: [],
+            is_template: inputs.template,
+          },
         },
-      },
-    })
+      })
 
-    reset()
+      reset()
+    } catch (error) {
+      console.error('An error occurred during form submission:', error)
+      setErrorMessage('An error occurred. Please try again.')
+    }
   }
 
   return (
-    <>
+    <Preloader loading={loading} error={error}>
       <Dialog open={opened} onClose={close}>
         <StyledDialogTitle>
           Create CV
@@ -68,9 +81,10 @@ const CreateCvForm: React.FC<CreateCvFormProps> = ({ close, confirm, opened }) =
               Save
             </Button>
           </StyledBox>
+          {errorMessage && <Notifications message={errorMessage} show={true} />}
         </DialogContent>
       </Dialog>
-    </>
+    </Preloader>
   )
 }
 
--- src/components/dialogs/language/language.types.ts
+++ src/components/dialogs/language/language.types.ts
@@ -4,5 +4,5 @@ import { ILanguage } from '../../../interfaces/language.interface'
 export type LanguageFormValues = Omit<ILanguage, 'id'>
 
 export type LanguageProps = DialogProps & {
-  item?: ILanguage
+  languageItem?: ILanguage
 }
