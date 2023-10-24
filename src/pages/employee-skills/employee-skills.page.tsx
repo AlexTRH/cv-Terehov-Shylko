bd79819be21c4d5fa753307c162ebbc4e0da83df
AlexTRH <19tam99@mail.ru>
2023-10-24 12:49:50 +0300

    Fixed according to comments + added Preloader

--- src/pages/employee-skills/employee-skills.page.tsx
+++ src/pages/employee-skills/employee-skills.page.tsx
@@ -4,11 +4,12 @@ import { useQuery } from '@apollo/client'
 import { Chip, CircularProgress, Typography } from '@mui/material'
 import { UserResult } from '@graphql/users/users.types'
 import { getUserQuery } from '@graphql/users/users.queries'
+import Preloader from '@atoms/preloader/preloader.atom'
 import * as Styled from './employee-skills.styles'
 
 const EmployeeSkills = () => {
   const { id } = useParams()
-  const { data, loading } = useQuery<UserResult>(getUserQuery, {
+  const { data, loading, error } = useQuery<UserResult>(getUserQuery, {
     variables: { id },
   })
 
@@ -21,7 +22,7 @@ const EmployeeSkills = () => {
   }
 
   return (
-    <>
+    <Preloader loading={loading} error={error}>
       {data?.user.profile.skills.map(({ skill_name, mastery }) => (
         <Styled.Skill key={skill_name}>
           <Typography variant="body1" sx={{ mr: 2 }}>
@@ -30,7 +31,7 @@ const EmployeeSkills = () => {
           <Chip label={mastery} variant="outlined" size="small" />
         </Styled.Skill>
       ))}
-    </>
+    </Preloader>
   )
 }
 
